0cc43c7716ee191b22d76874923b3af76d7b57ce
Berkay Polat <poberkaypo85@hotmail.com>
2023-03-02 07:01:47 +0300

    Changes from the first lesson

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {GiHamburgerMenu} from "react-icons/gi";
 
 export default function Navbar() {
   return (
-    <div className="heading">
+    <>
+        <div className="heading">
         <h1> Hello </h1>
     </div>
+    <GiHamburgerMenu />
+    </>
   )
 }
 
